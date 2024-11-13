ad90c69f568b3964c36ffa9c3340f93913cb8e76
Reidel Rojas <re94e12@gmail.com>
2024-11-12 21:42:22 -0500

    Subiendo nuevo archivo

---
+++ offline.js
@@ -0,0 +1,39 @@
+(async()=>{
+  try {
+
+    let user = api.user || {
+      paths: []
+    }
+
+    let list = ''
+    let n = 0
+    for (var i in offline) {
+      n++
+      list = `<a data-href="get/${i}" class="blog"><img src="${imgLoad}" data-src="${offline[i].img || 'http://png.txt/' + offline[i].title[0]}">${offline[i].title.slice(0, 50)} ${!(user.paths || []).includes(i) ? `<span data-path="${i}" onclick="hDel(this)" class="bi-trash"></span>` : ''}</a>` + list
+    }
+
+    hDel = async(o)=>{
+      event.stopPropagation()
+      event.preventDefault()
+      let path = o.dataset.path
+      accion('Eliminar "' + path + '" del historial', async()=>{
+        o.parentNode.remove()
+        delete offline[path]
+        await mydb.put('offline', offline)
+        await mydb.delete(path)
+      })
+    }
+
+    inner(`
+      <header static>
+       <h1><a data-href="account" class="bi-arrow-left-square-fill"> offline</a></h1>
+      </header>
+      <article>
+        ${list}
+      </article>
+    `)
+
+  } catch (err) {
+    eCatch(err.message)
+  }
+})()
